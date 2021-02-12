261f24643e52f1211bce8a08c662994e296e9114
NikitinaMaria <71255897+NikitinaMaria@users.noreply.github.com>
2021-02-12 17:28:50 +0300

    Add files via upload

---
+++ zadanie_8.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // Data entry
+    cout << "Enter a in ax = b (mod m):" << endl;
+    int a;
+    cin >> a;
+
+    cout << "Enter b in ax = b (mod m):" << endl;
+    int b;
+    cin >> b;
+
+    cout << "Enter m in ax = b (mod m):" << endl;
+    int m;
+    cin >> m;
+
+    int x;
+    int check = 0;
+
+    for (x = 0; x <= m; x++) {
+        if ((a * x) % m == b) {
+            check = 1;
+            cout << "x = " << x << endl;
+        }
+    }
+
+    if (check == 0) {
+        cout << -1 << " (There are no solutions)" << endl;
+    }
+    return 0;
+}
